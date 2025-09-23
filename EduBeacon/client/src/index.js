d617bbef904f14537896866b4aeda00ee3fe13a7
Nezareeen <nezareeen@gmail.com>
2025-09-23 17:18:57 +0530

    BG has been changed

--- EduBeacon/client/src/index.js
+++ EduBeacon/client/src/index.js
@@ -13,6 +13,9 @@ const BASE_URL =
 
 axios.defaults.baseURL = BASE_URL;
 
+// All the code related to the old background's pointer tracking has been removed.
+// The new canvas component handles its own animations.
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
